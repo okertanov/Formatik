acb5906957c5997917b50f04fd87d3438bd345f3
okertanov <okertanov@502b8fc9-1597-45f2-a909-db17c0c7c997>
2011-02-11 15:16:38 +0000

    initial api; router redesign;
    git-svn-id: svn+ssh://crater.espectrale.com/var/svn/repo/trunk/Formatik@283 502b8fc9-1597-45f2-a909-db17c0c7c997

--- public/server.js
+++ public/server.js
@@ -9,8 +9,7 @@ var sys  = require('sys'),
     path = require('path'),
     fs   = require('fs');
 
-const WEBROOT       = '/cygdrive/c/okertanov/projects/_unsupported_/Formatik.svn/public/';
-//const WEBROOT       = '/home/okertanov/projects/Formatik.svn/webroot/';
+const WEBROOT       = path.join(path.dirname(__filename), '/');
 const WEBPORT       = 8800;
 const INDEXHTML     = 'index.html';
 
@@ -23,13 +22,7 @@ http.createServer(function(request, response)
     catch(e)
     {
         sys.puts('Exception: ' + e);
-        var vDebug = ""; 
-        for (var prop in e) 
-        {   
-           vDebug += "property: "+ prop + " value: ["+ e[prop]+ "]\n"; 
-        } 
-        vDebug += "toString(): " + " value: [" + e.toString() + "]"; 
-        sys.puts('Exception details:\n ' + vDebug);
+        dump_js_object(e);
     }
 }).listen(WEBPORT);
 
@@ -37,17 +30,24 @@ sys.puts('Starting server at http://localhost:8800/...');
 
 function serve_request(request, response)
 {
-    switch (url.parse(request.url).pathname) 
+    var serve_path = url.parse(request.url).pathname;
+
+    //The top-level router
+    if (/^\/{1}(api)\/?(.*)$/i.test(serve_path)) // "/api", "/api/send/etc"
     {
-        case '/':
-          serve_request_fsobject(INDEXHTML, response);
-          break;
-        case '/api':
-          serve_request_api(request, response);
-          break;
-        default:
-          serve_request_fsobject(request, response);
-          break;
+        serve_request_api(request, response); 
+    }
+    else if (/^\/?$/.test(serve_path)) // "/"
+    {
+        serve_request_fsobject(INDEXHTML, response); 
+    }
+    else if (/^\/{1}(.*(?!api))$/i.test(serve_path)) // "/resource.mime"
+    {
+        serve_request_fsobject(request, response); 
+    }
+    else
+    {
+        throw "serve_request: Can't handle " + serve_path;
     }
 }
 
@@ -55,32 +55,70 @@ function serve_request_fsobject(request, response)
 {
     var url_obj = ( typeof request == 'object' ? url.parse(request.url).pathname : request );
     var filename = path.join(WEBROOT, url_obj);
+    var file_mime = get_mime_for(filename);
 
     path.exists(filename, function(exists) 
     {
         if(!exists) 
         {
-            return response_error(request, response, 404);
+            return response_http_code(request, response, 404);
+        }
+
+        if ( !file_mime.length || file_mime == '_disabled' )
+        {
+            return response_http_code(request, response, 403);
         }
 
         fs.readFile(filename, "binary", function(err, file) 
         {
             if(err) 
             {
-                return response_error(request, response, 500);
+                return response_http_code(request, response, 500);
             }
 
-            response.writeHead(200, {"Content-Type": get_mime_for(filename)});
+            response.writeHead(200, {"Content-Type": file_mime});
             response.write(file, "binary");
             response.end();
-            sys.puts(url_obj + ': \t\t200 OK');
+            sys.puts(url_obj + ': \t\t200 OK' + ' (' + file_mime + ')');
         });
     });
 }
 
 function serve_request_api(request, response)
 {
-    return response_error(response, 501);
+    var api_path = url.parse(request.url).pathname;
+    var api_re = /^\/{1}(api)\/{0,1}(.*)$/i;
+    var api_result = api_re.exec(api_path);
+    var api_api = api_result[1] || '';
+    var api_cmd = api_result[2] || '';
+
+    try
+    {
+        if (!api_api.length || !api_cmd.length)
+            throw 'API Exception: api_api or api_cmd couldn\'t be empty.';
+        switch(api_cmd)
+        {
+            case 'ping':
+                response_http_code(request, response, 200);
+                break;
+            case 'newtask':
+                break;
+            case 'settings':
+                break;
+            case 'auth':
+                response_http_code(request, response, 401);
+                break;
+            default:
+                throw 'API Exception: unknown api command.';
+                break;
+        }
+        sys.puts('\tAPI>>> ' + api_api + ' cmd: ' + api_cmd);
+    }
+    catch(e)
+    {
+        sys.puts('Exception: ' + e);
+        response_http_code(request,response, 501);
+    }
 }
 
 var HTTPMimeTypes = 
@@ -89,13 +127,16 @@ var HTTPMimeTypes =
     'html': 'text/html',
     'css' : 'text/css',
     'js'  : 'application/javascript', 
-    'ico' : 'image/x-icon'
+    'ico' : 'image/x-icon', 
+    'json': 'application/json',
+    '---' : '_disabled'
 };
 
 function get_mime_for(filename)
 {
-    var extension = filename.match(/\.(\w+)$/)[1];
-    var mime = HTTPMimeTypes['text'];
+    var extension_re_result = filename.match(/\.(\w+)$/);
+    var extension = (extension_re_result?extension_re_result[1]:'');
+    var mime = HTTPMimeTypes['---'];
     switch (extension)
     {
         case 'htm':
@@ -115,7 +156,7 @@ function get_mime_for(filename)
             mime = HTTPMimeTypes['ico'];
             break;
         default:
-            mime = HTTPMimeTypes['text'];
+            mime = HTTPMimeTypes['---'];
             break;
     }
     return mime;
@@ -124,17 +165,31 @@ function get_mime_for(filename)
 var HTTPErrorStrings = 
 {
     200: 'OK',
+    401: 'Unauthorized',
+    403: 'Forbidden', 
     404: 'Not Found', 
     500: 'Internal Server Error', 
-    501: 'Not Implemented' 
+    501: 'Not Implemented'
 };
 
-function response_error(request, response, errcode)
+function response_http_code(request, response, errcode)
 {
     var url_obj = ( typeof request == 'object'? url.parse(request.url).pathname : request );
     response.writeHead(errcode, {"Content-Type": HTTPMimeTypes['text']});
     response.write(errcode + ' ' + HTTPErrorStrings[errcode] + '\n');
     response.end();
     sys.puts(url_obj + ': \t\t' + errcode + ' ' + HTTPErrorStrings[errcode]);
+}
+
+function dump_js_object(obj)
+{
+    var str_out = ""; 
+    for (var prop in obj) 
+    {   
+        str_out += "\tproperty: "+ prop + " value: ["+ obj[prop]+ "]\n"; 
+    } 
+    
+    str_out += "toString(): " + " value: [" + obj.toString() + "]"; 
+    sys.puts('JS Object details for:\n ' + obj + "\n" + str_out);
 }
 
